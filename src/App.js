faeb7284d87ca9a3c4ea1dcd23f85aaf0eea554d
igaboo <dw.jaden@gmail.com>
2022-06-27 14:45:20 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useEffect, useRef, useState } from "react";
+
+import io from "socket.io-client";
+
+const socket = io.connect("http://10.0.0.77:3001");
 
 function App() {
+  const messageRef = useRef();
+
+  const [message, setMessage] = useState("");
+
+  const [history, setHistory] = useState([]);
+
+  function sendMessage() {
+    socket.emit("send_message", { message: messageRef.current.value });
+    setHistory([
+      { message: messageRef.current.value, type: "sent" },
+      ...history,
+    ]);
+  }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessage(data.message);
+    });
+
+    //eslint-disable-next-line
+  }, [socket]);
+
+  useEffect(() => {
+    if (message !== "") setHistory([{ message, type: "received" }, ...history]);
+    //eslint-disable-next-line
+  }, [message]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <input ref={messageRef} type="text" placeholder="Message" />
+        <button onClick={sendMessage}>Send</button>
       </header>
+
+      <div className="container">
+        {history.map((msg, i) => {
+          return (
+            <h2 key={i} className={msg.type}>
+              {msg.message}
+            </h2>
+          );
+        })}
+      </div>
     </div>
   );
 }
